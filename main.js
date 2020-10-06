c9d6cd47ea99c4ed79f2d25d6bb8c65e5d0edeae
classAndrew <andrxwlin@gmail.com>
2020-10-05 17:07:37 -0700

    Now renders the mandelbrot fractal and zooms

--- main.js
+++ main.js
@@ -1,19 +1,38 @@
 const vsSource = `#version 300 es
 layout (location = 0) in vec4 aVertexPosition;
-
-uniform mat4 uModelViewMatrix;
-uniform mat4 uProjectionMatrix;
-
+out vec4 vPos;
+uniform float xshift;
 void main() {
-  gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+    vPos = aVertexPosition;
+    vPos.y /= xshift;
+    vPos.x /= xshift;
+    vPos.x -= 0.72;
+    vPos.y -= 0.202;
+    gl_Position = aVertexPosition;
 }
 `;
 
 const fsSource = `#version 300 es
 precision mediump float;
+
+in vec4 vPos;
 out vec4 FragColor;
 void main() {
-  FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    vec2 z = vPos.xy;
+    int i = 0;
+    int lim = 200;
+    for (; i < lim; ++i) {
+        z = vec2(z.x*z.x-z.y*z.y+vPos.x, 2.0f*z.x*z.y+vPos.y);
+        if (distance(z, vec2(0.0f,0.0f)) > 2.0f) {
+            break;
+        }
+    }
+    if (i == lim) {
+        FragColor = vec4(0.0f, 0.0f, 0.0f, 1.0f);
+    } else {
+        FragColor = vec4(float(i)/float(lim), float(i)/float(lim), float(i)/float(lim), 1.0f)*vec4(0.1f, 0.9f, 0.9f, 1.0f);
+    }
+    
 }
 `;
 
@@ -61,7 +80,7 @@ function main() {
 
     const vbuf = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vbuf);
-    const vertices = [0, 0, 1, 0, 0, 1, 1, 1];
+    const vertices = [-1, -1, 1, -1, -1, 1, 1, 1];
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
     beginRender(gl, shadProgram, vbuf);
 }
@@ -85,23 +104,16 @@ function beginRender(gl, progshad, buff) {
     }
     requestAnimationFrame(render);
 }
-var rotation = 0;
+var rotation = 1;
 
 function draw(gl, dt, model, proj, buff, progshad) {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    mat4.perspective(proj, 45 * 3.141592653589 / 180, gl.canvas.clientWidth / gl.canvas.clientHeight, 0.1, 100);
-
-    model = mat4.create();
-
-    mat4.translate(model, model, [0, 0, -10]);
-    mat4.rotate(model, model, rotation += dt * 0.01, [0, 1, 0]);
     gl.bindBuffer(gl.ARRAY_BUFFER, buff);
     gl.vertexAttribPointer(0, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(0);
+    gl.uniform1f(gl.getUniformLocation(progshad, "xshift"), rotation *= 1.02);
     gl.useProgram(progshad);
-    gl.uniformMatrix4fv(gl.getUniformLocation(progshad, "uProjectionMatrix"), false, proj);
-    gl.uniformMatrix4fv(gl.getUniformLocation(progshad, "uModelViewMatrix"), false, model);
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 }
 
